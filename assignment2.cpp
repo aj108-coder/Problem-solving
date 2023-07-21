2d2193a51a58672fab050766184e302948a1dc9a
Anshul Jain <139606790+aj108-coder@users.noreply.github.com>
2023-07-22 00:31:41 +0530

    Update assignment2.cpp

--- assignment2.cpp
+++ assignment2.cpp
@@ -32,15 +32,16 @@ int base_calculation(int num)
 
 int main()
 {
-    int n;
+   long int n; /// size of array 
         cin>>n;
-    
-    int* arr = new int[n];
+     
+     int* arr = new int[n]; // Dynamically allocated array uses heap and the adress store at stack  :-> it restrict the stack overflow
 
+//take input random numbers
 cout<<"Enter the elements of array : ";
     for(int i = 0; i<n ; i++)
     {
-        arr[i] = rand() % 1000;
+        arr[i] = rand() % 10000000000 + 1000000;
 
         // cin>> arr[i];
 
@@ -51,17 +52,17 @@ cout<<"Given array : ";
     print(arr,n);
 
     cout<<endl;
-// count[] :-> nice array which stores number of lement respective their base
+// count[] :-> nice array which stores number of element respective their base
 
 int* count = new int[16];
 
 for(int i = 0 ;i < 16 ; i++ )
 {
-    count[i] = 0;
+    count[i] = 0; // if we do not intialise the any variable ,then compiler assign garbage value to the variable
 }
 
 
-    
+//Here we store the base and incremment the index according to base
 for(int i = 0 ;i<n ;i++ )
 {
     int base = base_calculation(arr[i]);
@@ -79,7 +80,7 @@ for(int i = 0 ;i<16 ; i++)
 
     cout<<endl;
 
-for(int i=1 ; i<16 ; i++)
+for(int i=1 ; i<16; i++)
 {
     cout<<"Base "<<i<<": "<<count[i]<<endl;
 }
@@ -99,11 +100,11 @@ cout<<"Graphical representation in star pattern : "<<endl;
 int maxNumber = 0;
     for (int i = 1; i < 16; i++) {
         if (count[i] > maxNumber) {
-            maxNumber = count[i];
+            maxNumber = count[i];   // maximum number helps to find the number of maximum coloumn
         }
     }
 
-    for (int i = maxNumber; i > 0; i--) {
+    for (int i = maxNumber; i > 0; i--) { //this print stars in coloumn
         for (int j = 1; j < 16; j++) {
             if (count[j] >= i) {
                 cout << "* ";
@@ -115,10 +116,11 @@ int maxNumber = 0;
     }
 
     for (int i = 1; i < 16; i++) {
-        cout << i << " ";
+        cout << i << " ";       // this loop prints the numbers horizontally
     }
     cout << endl;
 
+//free up the dynamically allocated memory : if do not free it then it would be a responsible for memory lekage which is bad efffect on code
     delete[] count;
     delete[] arr;
 
