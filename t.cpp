1dadb7e4b899ce8cecdcd91f69de5a87c2e6d0f0
Anshul Jain <139606790+aj108-coder@users.noreply.github.com>
2023-07-24 20:20:52 +0530

    assignment1_sec_A_by_jaski_sir.cpp

--- t.cpp
+++ t.cpp
@@ -8,19 +8,21 @@
 
 using namespace std;
 
-int base_calculation(string str);
+//use of sunction increases the modularity and readability
 
-string generateRandomHexadecimal(int length) ;
+int base_calculation(string str);  // calculate base of each hexadeimal number
 
-
-bool is_input_hexadecimal(string str);
-
-void input(string* arr, int size_of_array);
-
-void print(string* arr, int size_of_array);
+string generateRandomHexadecimal(int length) ;  // geenerate random hexadecimal numbers
 
 
-int main()
+bool is_input_hexadecimal(string str); // HANDLE INPUT TEST CASE :->if someone enters the wrong output it returns false
+
+void input(string* arr, int size_of_array);  // it just take input the data -->hexadcimal numbers in array
+
+void print(string* arr, int size_of_array); // it just print the given output
+
+
+int main()  // starting point of c/c++ code :->call by O.S.
 {
 int size_of_array;
     cout << "Input the quantity of hexadecimal number to find the frequency of base : " << endl;
@@ -183,6 +185,8 @@ void print(string* arr, int size_of_array)
 }
 int base_calculation(string str)
 { 
+
+    // through this we calculate the base of each hexadecimal value
     int base = 0 ;
    char last_char= 0 ; 
   int n = str.length();
@@ -191,13 +195,13 @@ int base_calculation(string str)
 
    while( !str.empty())
    {
-       last_char = str.back();
+       last_char = str.back(); //fetch the last eleement of string
 
  
 
          if ( last_char >= 'A' && last_char <= 'F')
         {
-            last_integer[right--] = last_char - 'A' + 10;
+            last_integer[right--] = last_char - 'A' + 10;  //convert a character into a intger
 
         }
 
@@ -207,10 +211,10 @@ int base_calculation(string str)
         last_integer[right--] = last_char - '0';
      }
        
-         str.pop_back();
+         str.pop_back();  //remove the last element of string
    }
 
-   int max = INT_MIN ;
+   int max = INT_MIN ;  // intailse the max with INT_MIN(smallest number in the range of integer data type in c++)
 
    for( int  i = 0 ; i < n ; i++)
    {
@@ -220,7 +224,7 @@ int base_calculation(string str)
    }
 
    base = max;
-    return (base+1);
+    return (base+1); // base of hexadecimal number = maximum _number + 1 :-> ex : 123AB :-> maximum_nmber = B (11) :-> BASE = 11+1 = 12
 }
 
 string generateRandomHexadecimal(int length) {
@@ -238,3 +242,99 @@ string generateRandomHexadecimal(int length) {
 
     return result;
 }
+
+
+
+//BELOW INPUT AND OUTPTUT IS GIVEN  { OUTPTUT WINDOW }
+/* 
+Input the quantity of hexadecimal number to find the frequency of base : 
+45
+1)USER-INPUT
+2)Randomly generated hexadecimal values 
+Enter your choice : 1
+Enter the 45 hexadecimal values: 
+1
+1
+1
+2
+2
+2
+3
+3
+3
+4
+4
+4 
+5
+5
+5
+6
+6
+6
+7
+7
+7
+8
+8
+8
+9
+9
+9
+A 
+A
+A
+B
+B
+B
+C
+C
+C
+D
+D
+D
+E
+E
+E
+F
+F
+F
+
+Given array:
+1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9 A A A B B B C C C D D D E E E F F F 
+0 0 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+Base 0: 0
+Base 1: 0
+Base 2: 3
+Base 3: 3
+Base 4: 3
+Base 5: 3
+Base 6: 3
+Base 7: 3
+Base 8: 3
+Base 9: 3
+Base 10: 3
+Base 11: 3
+Base 12: 3
+Base 13: 3
+Base 14: 3
+Base 15: 3
+Base 16: 3
+
+Graphical representation in star pattern :
+ 1:
+ 2: * * *
+ 3: * * *
+ 4: * * *
+ 5: * * *
+ 6: * * *
+ 7: * * *
+ 8: * * *
+ 9: * * *
+10: * * *
+11: * * *
+12: * * *
+13: * * *
+14: * * *
+15: * * *
+16: * * *
+*/
